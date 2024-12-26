80bc24c44fbfbc4f429e5cd88f444c964466ceae
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-12-26 16:39:12 -0500

    feat: add initial colors and font

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,35 @@ export default function starlightThemeBlack(): StarlightPlugin {
             ],
             logger,
           ),
-          customCss: [...(config.customCss ?? []), 'starlight-theme-black/styles'],
+          customCss: [
+            ...(config.customCss ?? []),
+            '@fontsource/geist-mono/100.css',
+            '@fontsource/geist-mono/200.css',
+            '@fontsource/geist-mono/300.css',
+            '@fontsource/geist-mono/400.css',
+            '@fontsource/geist-mono/500.css',
+            '@fontsource/geist-mono/600.css',
+            '@fontsource/geist-mono/700.css',
+            '@fontsource/geist-mono/800.css',
+            '@fontsource/geist-mono/900.css',
+            '@fontsource/geist-sans/100.css',
+            '@fontsource/geist-sans/200.css',
+            '@fontsource/geist-sans/300.css',
+            '@fontsource/geist-sans/400.css',
+            '@fontsource/geist-sans/500.css',
+            '@fontsource/geist-sans/600.css',
+            '@fontsource/geist-sans/700.css',
+            '@fontsource/geist-sans/800.css',
+            '@fontsource/geist-sans/900.css',
+            'starlight-theme-black/styles',
+          ],
+          expressiveCode:
+            config.expressiveCode === false
+              ? false
+              : {
+                  themes: ['github-dark-default', 'github-light-default'],
+                  ...(typeof config.expressiveCode === 'object' ? config.expressiveCode : {}),
+                },
         })
       },
     },
