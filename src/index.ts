8bed526f2381b2e9ce1bbdf1138039ce50817ae7
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-12-28 00:19:41 -0500

    feat: add Footer component override

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export default function starlightThemeBlack(): StarlightPlugin {
               'PageTitle',
               'MarkdownContent',
               'Hero',
+              'Footer',
             ],
             logger,
           ),
