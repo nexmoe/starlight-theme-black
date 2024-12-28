d2b2e3174b58615e67194c96c911b914b1b161a9
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-12-27 23:50:00 -0500

    feat: add Hero component override and update layout structure

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ export default function starlightThemeBlack(): StarlightPlugin {
               'ContentPanel',
               'PageTitle',
               'MarkdownContent',
+              'Hero',
             ],
             logger,
           ),
