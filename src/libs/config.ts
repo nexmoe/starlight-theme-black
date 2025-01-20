37851c67aa8dfee85743eb033ad44ae8fcc7f599
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-01-19 23:01:48 -0500

    feat: add footer text config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -5,6 +5,7 @@ export const StarlightThemeBlackConfigSchema = z.object({
     text: z.string(),
     slug: z.string(),
   })).optional(),
+  footerText: z.string().optional().default('Built & designed by [shadcn](https://twitter.com/shadcn). Ported to Astro Starlight by [Adrián UB](https://github.com/adrian-ub). The source code is available on [GitHub](https://github.com/adrian-ub/starlight-theme-black).'),
 })
 
 export type StarlightThemeBlackUserConfig = z.input<typeof StarlightThemeBlackConfigSchema>
