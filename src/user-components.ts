c4fda165193399f7d7c713f2b9dc825ab1fa33a5
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2025-01-26 22:16:18 -0500

    feat: add LinkedCard and LinkedCardGroup components for enhanced card layout

--- src/user-components.ts
+++ src/user-components.ts
@@ -1,3 +1,5 @@
 export { default as Callout } from './components/Callout.astro'
 export { default as ContainerSection } from './components/ContainerSection.astro'
+export { default as LinkedCard } from './components/LinkedCard.astro'
+export { default as LinkedCardGroup } from './components/LinkedCardGroup.astro'
 export { default as Steps } from './components/Steps.astro'
